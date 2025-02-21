18ba59bd2804840a551276388e83e075c04fc05d
AlexDev <moronturielalejandro@gmail.com>
2025-02-21 19:12:02 +0100

    navbar user part and cart icon seted

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbDropdownModule, NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [NgbNavModule, NgbDropdownModule,NgbModule],
+  imports: [NgbNavModule, NgbDropdownModule,NgbModule,FontAwesomeModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
   isCollapsed = true;
   activeLink: string = '';
+  userimg: string = "http://localhost:8080/data/imagenes/user.png";
 
   constructor(private router: Router) { }
   
