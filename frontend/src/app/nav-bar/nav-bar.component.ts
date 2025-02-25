8aa0731508135d9ccfc9ab77fd9fba6b4065b4a1
AlexDev <moronturielalejandro@gmail.com>
2025-02-25 13:41:47 +0100

    slider block for carrito seted

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, WritableSignal, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbDropdownModule, NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepickerModule, NgbDropdownModule, NgbModule, NgbNavModule, NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [NgbNavModule, NgbDropdownModule,NgbModule,FontAwesomeModule],
+  imports: [NgbNavModule, NgbDropdownModule, NgbModule, FontAwesomeModule, NgbDatepickerModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
@@ -16,10 +16,35 @@ export class NavBarComponent {
   authState: string = "Registro/Login";
 
   constructor(private router: Router) { }
-  
+
   route(path: string): void {
     this.router.navigate([path]);
     this.activeLink = path;
+  }
+
+  private offcanvasService = inject(NgbOffcanvas);
+  closeResult: WritableSignal<string> = signal('');
+
+  open(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
+      (result) => {
+        this.closeResult.set(`Closed with: ${result}`);
+      },
+      (reason) => {
+        this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+      },
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case OffcanvasDismissReasons.ESC:
+        return 'by pressing ESC';
+      case OffcanvasDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on the backdrop';
+      default:
+        return `with: ${reason}`;
+    }
   }
 }
 
